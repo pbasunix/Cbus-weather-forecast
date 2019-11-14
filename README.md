f5de1a2ba3d9d4338f81f340a375ddfab717a8f2
roma1927 <pba.sunix@gmail.com>
2019-11-14 12:29:06 -0500

    Scrape weather forecast:  day, temp and description && Create panda dataframe and display result in table

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cbus-weather-forecast
+
+Simple script to track the seven day forecast for the city of Columbus and with the zip-code of 43220
+
+
--- cbus_weathercast.py
+++ cbus_weathercast.py
@@ -1,15 +1,22 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 URL = requests.get("https://forecast.weather.gov/MapClick.php?lat=40.051030000000026&lon=-83.07007999999996#.Xc18oFFKjPY")
 
 soup = BeautifulSoup(URL.content, 'html.parser')
-
 seven_day_forecast = soup.find(id="seven-day-forecast-body")
-#print(seven_day_forecast)
 
 weather_objects = (seven_day_forecast.find_all(class_='tombstone-container'))
 
-print(weather_objects)
+day = [day.find(class_='period-name').get_text() for day in weather_objects]
+desc = [desc.find(class_='short-desc').get_text() for desc in weather_objects]
+temp = [temp.find(class_='temp').get_text() for temp in weather_objects]
 
+weekly_cast = pd.DataFrame({
+    'day': day,
+    'desc': desc,
+    'temp': temp,
+})
 
+print(weekly_cast)
